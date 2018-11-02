a3353b6bcae2edf71f0ac7854f4aea9825c3e9b6
balabalaQAQ <1124048070@qq.com>
2018-11-02 11:19:30 +0800

    添加项目文件。

---
+++ demo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new CourseDBEntities().Courses.ToList();
+        }
+    }
+}
