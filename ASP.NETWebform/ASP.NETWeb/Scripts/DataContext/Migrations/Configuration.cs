fcd52f4de6adbc740439ad32f9b8e14bf58068d2
balabalaQAQ <1124048070@qq.com>
2018-11-11 16:10:10 +0800

    .

--- ASP.NETWebform/ASP.NETWeb/Scripts/DataContext/Migrations/Configuration.cs
+++ ASP.NETWebform/ASP.NETWeb/Scripts/DataContext/Migrations/Configuration.cs
@@ -1,10 +1,9 @@
-namespace DataContext.Migrations
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+namespace DataContext
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
     internal sealed class Configuration : DbMigrationsConfiguration<DataContext.StuDBContext>
     {
         public Configuration()
--- ASP.NETWebform/ASP.NETWeb/Scripts/DataContext/Migrations/DepartMentSeed.cs
+++ ASP.NETWebform/ASP.NETWeb/Scripts/DataContext/Migrations/DepartMentSeed.cs
@@ -1,9 +1,11 @@
-﻿
-using Entities;
+﻿using Entities;
+using EntityFramework.Extensions;
+using MoreLinq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DataContext
