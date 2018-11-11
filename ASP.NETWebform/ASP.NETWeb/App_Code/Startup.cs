388b8e5ed9a15fccaace9f5e7c743ffa96b0dde8
balabalaQAQ <1124048070@qq.com>
2018-11-11 14:53:26 +0800

    news

---
+++ ASP.NETWebform/ASP.NETWeb/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NetWeb.Startup))]
+namespace ASP.NetWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
