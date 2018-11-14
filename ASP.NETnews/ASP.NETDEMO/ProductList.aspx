0b682689b2b0a0563ecf04a99241fc715481bead
balabalaQAQ <1124048070@qq.com>
2018-11-14 17:23:01 +0800

    1000学生编辑

--- ASP.NETnews/ASP.NETDEMO/ProductList.aspx
+++ ASP.NETnews/ASP.NETDEMO/ProductList.aspx
@@ -1,9 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="ProductList.aspx.cs" Inherits="ProductList" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
-    <h4 style="text-align:center">商品列表<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
-    </h4>
-    <asp:GridView ID="GridView1"  runat="server" AllowPaging="True" AutoGenerateColumns="False" DataKeyNames="ID" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowDeleting="GridView1_RowDeleting" PageSize="15" HorizontalAlign="Center" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">
+    <h4 style="text-align:center">商品列表</h4>
+    <asp:GridView ID="GridView1"  runat="server" AllowPaging="True" AutoGenerateColumns="False" DataKeyNames="ID" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowDeleting="GridView1_RowDeleting" PageSize="15" HorizontalAlign="Center" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
         <Columns>
             <asp:BoundField DataField="StudentCode" HeaderText="学号">
             <ItemStyle Width="150px" />
@@ -14,9 +13,16 @@
             <asp:BoundField DataField="Phone" HeaderText="电话">
             <ItemStyle Width="150px" />
             </asp:BoundField>
-            <asp:BoundField HeaderText="所属学院">
-            <ItemStyle HorizontalAlign="Center" Width="150px" />
-            </asp:BoundField>
+            <asp:TemplateField HeaderText="所属学院">
+                <EditItemTemplate>
+                    <asp:DropDownList ID="editdepartment" runat="server" Height="30px" Width="150px">
+                    </asp:DropDownList>
+                </EditItemTemplate>
+                <ItemTemplate>
+                    <asp:Label ID="Label1" runat="server" Text='<%# GetName(Eval("Department")) %>'></asp:Label>
+                </ItemTemplate>
+                <ItemStyle HorizontalAlign="Center" Width="150px" />
+            </asp:TemplateField>
             <asp:TemplateField HeaderText="操作" ShowHeader="False">
                 <EditItemTemplate>
                     <asp:LinkButton ID="LinkButton1" runat="server" CausesValidation="True" CommandName="Update" Text="更新"></asp:LinkButton>
--- ASP.NETnews/ASP.NETDEMO/ProductList.aspx.cs
+++ ASP.NETnews/ASP.NETDEMO/ProductList.aspx.cs
@@ -24,6 +24,7 @@ public partial class ProductList : System.Web.UI.Page
             GridView1.DataBind();
         }
     }
+    //删除
     protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
     {
         var id = Guid.Parse(GridView1.DataKeys[e.RowIndex].Value.ToString());
@@ -38,11 +39,11 @@ public partial class ProductList : System.Web.UI.Page
         _getData();
     }
 
-
-
+    //翻页
     protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
     {
         GridView1.PageIndex = e.NewPageIndex;
+        GridView1.EditIndex = -1;
         _getData();
     }
 
@@ -63,30 +64,55 @@ public partial class ProductList : System.Web.UI.Page
             //获取用户编辑的这一行
             var row = GridView1.Rows[e.RowIndex];
             var sn = (row.Cells[0].Controls[0] as TextBox).Text.Trim();
-            var name = (row.Cells[1].Controls[1] as TextBox).Text.Trim();
-            var phone = (row.Cells[2].Controls[2] as TextBox).Text.Trim();
-            var department = (row.Cells[3].Controls[3] as TextBox).Text.Trim();
+            var name = (row.Cells[1].Controls[0] as TextBox).Text.Trim();
+            var phone = (row.Cells[2].Controls[0] as TextBox).Text.Trim();
+
+            var department = Guid.Parse(((DropDownList)row.FindControl("editdepartment")).SelectedValue);
             p.StudentCode = sn;
             p.Name = name;
             p.Phone = phone;
-            p.Department = context.Departments.Single(x => x.Name == department);
+            p.Department = context.Departments.Find(department);
             context.SaveChanges();
         }
         GridView1.EditIndex = -1;
         _getData();
     }
-
+    //切换到编辑
     protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
     {
         GridView1.EditIndex = e.NewEditIndex;
         _getData();
+        //查询出所有的分类
+        var context = new DataContext.StuDBContext();
+        var departments = context.Departments.ToList();
+        //查询出gridview中分类列编辑状态模板中下拉菜单
+        var  list = (DropDownList)GridView1.Rows[e.NewEditIndex].FindControl("editdepartment");
+        //下拉数据绑定
+        list.DataSource = departments;
+        list.DataTextField = "Name";
+        list.DataValueField = "ID";
+        list.DataBind();
+
+        //选项绑定
+        var ID = (Guid)GridView1.DataKeys[e.NewEditIndex].Value;
+        //查询出当前记录商品记录，获取它的分类
+        var DTdata = context.Students.Find(ID);
+        if (DTdata.ID != null)
+            list.SelectedValue = DTdata.Department.ID.ToString();
+
+
     }
 
+    //分类字段绑定方法
     protected string GetName(object obj)
     {
-        if (obj !=null)
-            return((Department))
+        if (obj != null)
+            return ((Department)obj).Name;
+        return "该商品未分类";
     }
 
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
 
+    }
 }
