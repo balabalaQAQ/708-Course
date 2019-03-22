c5bc7f7a6fa8814e6cedd27feacc087d21ee5ee3
balabalaQAQ <1124048070@qq.com>
2019-03-23 01:23:41 +0800

    四则运算题

--- 数学题四则运算/Arithmetic/Arithmetic/Demand.cs
+++ 数学题四则运算/Arithmetic/Arithmetic/Demand.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Data;
+using System.IO;
+
 namespace Arithmetic
 {
 
@@ -15,6 +17,7 @@ namespace Arithmetic
         static string result = "";//结果
         static string Formula = "";//显示的文本
         static string Formulanum = "";//用于运算的文本
+        static Random number = new Random(); //实例化一个随机数  
         /// <summary>
         /// 判断输入的类型是否为整数
         /// </summary>
@@ -26,8 +29,7 @@ namespace Arithmetic
             {
                 return true;
             }
-                Console.WriteLine("请输入整数");
-                return false;
+            return false;
         }
         /// <summary>
         /// 问题验证
@@ -35,9 +37,9 @@ namespace Arithmetic
         /// <param name="input"></param>
         /// <param name="l"></param>
         /// <returns></returns>
-        public static bool IsNumber(string input, int l)
+        public static bool Problem(string input, int l)
         {
-            if (Regex.IsMatch(input, @"^[+-]?\d*[.]?$"))
+            if (IsNumber(input) == true)
             {
                 var num = Convert.ToDouble(input);
                 if (l == 1)
@@ -49,21 +51,29 @@ namespace Arithmetic
                 }
                 if (l == 2)
                 {
-                    if (num <= 1 || num > 100)
+                    if (num < 1 || num > 100)
                     {
                         Console.WriteLine("请输入正确的数据范围");
                     }
                 }
                 if (l == 3)
                 {
-                    if (num <= 1 || num > 3)
+                    if (num < 1 || num > 3)
                     {
                         Console.WriteLine("请输入正确范围内的运算符数量");
                     }
                 }
+                if (l == 4)
+                {
+                    if (num < 1 || num > 6)
+                    {
+                        Console.WriteLine("请输入正确的年级段");
+                    }
+                }
                 return true;
             }
-                return false;
+            Console.WriteLine("请输入整数");
+            return false;
         }
         /// <summary>
         /// 公式配置与运算
@@ -71,41 +81,32 @@ namespace Arithmetic
         /// <param name="Number">初始数</param>
         /// <param name="Range">数据范围</param>
         /// <param name="rule">运算符数量</param> 
-        public static void OpNumber(string Number, string Range,int rule)
+        /// <param name="Grade">年级</param> 
+        public static void OpNumber(int Number, int Range, int rule, int Grade)
         {
-            Random number = new Random(); //实例化一个随机数  
-            int num = Convert.ToInt32(Number);
-            int range = Convert.ToInt32(Range);
-            string[] myArray = new string[num];
-            DataTable dt = new DataTable();
-            for (int i = 1; i <= num; i++)
+
+            Range = Range + 1;
+            for (int i = 1; i <= Number; i++)
             {
-                double number1 = number.Next(0, range);//随机一个初始数
-                int OP = number.Next(1, rule+1);//随机运算符数量
-             
-                Formulanum = Formula = "";
-                Formulanum = Formula += number1;
-                for (int j = 1; j <= OP; j++)
-                {
-                    int opnext = number.Next(0, 4);//随机一个符号
-                    int number2 = number.Next(0, range);//随机一个数
-
-                    if (opnext == 3 && number2 == 0)//判断算式是否存在除0
-                    {
-                        number2 = number.Next(0, range);//重新随机一个数
-                    }
-                    int newOP = FormulaOP(opnext);
-
-                    Formula += op[newOP] + number2;
-                    Formulanum += ll[newOP] + number2;
-                }
-                result = dt.Compute(Formulanum, "false").ToString();
-                if (IsNumber(result, 6) == false)
+                FormulaSetup(Range, rule, Grade);
+                if (IsNumber(result) == false)
                 {
                     result = Convert.ToDouble(result).ToString("#0.00");  //使小数结果保留两位小数
                     Console.WriteLine(Formula + "≈" + result);
                 }
                 Console.WriteLine(Formula + "=" + result);
+                //生成题目
+                string Exercise = "txt/Exercise.txt";//创建txt文件的具体路径
+                StreamWriter exercise = new StreamWriter(Exercise, false, Encoding.Default);//实例化StreamWriter
+                exercise.WriteLine("1."+Formula);
+                exercise.Flush();
+                exercise.Close();
+                //生成答案
+                string Answer = "txt/Answer.txt";
+                StreamWriter answer = new StreamWriter(Answer, false, Encoding.Default);
+                answer.WriteLine("1." + result);
+                answer.Flush();
+                answer.Close();
             }
         }
         /// <summary>
@@ -130,7 +131,7 @@ namespace Arithmetic
             if (opnext == 3)//避免太多的除法
             {
                 l++;
-                if (opnext >1)
+                if (opnext > 1)
                 {
                     NewOpnext = number.Next(0, 4);
                     return NewOpnext;
@@ -146,6 +147,53 @@ namespace Arithmetic
         public static string Fraction()
         {
             return null;
+        }
+        /// <summary>
+        /// 表达式打印和输出
+        /// </summary>
+        /// <param name="Formula"></param>
+        /// <param name="Formulanum"></param>
+        public static void FormulaSetup(int Range, int rule, int Grade)
+        {
+            DataTable dt = new DataTable();
+            double number1 = number.Next(0, Range);//随机一个初始数
+            int OP = number.Next(1, rule + 1);//随机运算符数量
+            int opnext = 0; //符号
+            int number2 = 0; //数
+            Formulanum = Formula = "";
+            Formulanum = Formula += number1;
+            for (int j = 1; j <= OP; j++)
+            {
+                //使三年级前无乘除
+                if (Grade <= 2)
+                {
+                    opnext = number.Next(0, 2);
+                }
+                else
+                {
+                    opnext = number.Next(0, 4);
+                }
+                //随机一个符号
+                number2 = number.Next(0, Range);//随机一个数
+
+                int newOP = FormulaOP(opnext);
+                Formula += op[newOP] + number2;
+                Formulanum += ll[newOP] + number2;
+
+            }
+            result = dt.Compute(Formulanum, "false").ToString();
+            if (Convert.ToDouble(result) < 0 || (opnext == 3 && number2 == 0))//判断算式是否存在除0或小于0
+            {
+                FormulaSetup(Range, rule, Grade);
+            }
+            if (Grade < 4)
+            {
+                if (IsNumber(result) == false)//是4年级之前无法出现小数
+                {
+                    FormulaSetup(Range, rule, Grade);
+                }
+            }
+
         }
 
     }
--- 数学题四则运算/Arithmetic/Program.cs
+++ 数学题四则运算/Arithmetic/Program.cs
@@ -11,16 +11,19 @@ namespace Arithmetic
             sp.Start();
             Console.WriteLine("请输入出题量（1-100000）题");
             var number = Console.ReadLine();
-            var N = Demand.IsNumber(number, 1);
+            var N = Demand.Problem(number, 1);
             Console.WriteLine("请输入数据范围（1-100）");
             var Range = Console.ReadLine();
-            var L = Demand.IsNumber(Range, 2);
+            var L = Demand.Problem(Range, 2);
             Console.WriteLine("请输入运算符数量（1-3）个");
             var rule = Console.ReadLine();
-            var r = Demand.IsNumber(rule, 3);
-            if (N == L == r == true)
+            var r = Demand.Problem(rule, 3);
+            Console.WriteLine("请输入年级，以配置难度(1-6)年级");
+            var Grade = Console.ReadLine();
+            var G = Demand.Problem(Grade, 4);
+            if (N == L == r ==G== true)
             {
-                Demand.OpNumber(number, Range, Convert.ToInt32(rule));
+                Demand.OpNumber(Convert.ToInt32(number), Convert.ToInt32(Range), Convert.ToInt32(rule), Convert.ToInt32(Grade));
             }
             sp.Stop();
             Console.WriteLine(sp.Elapsed.ToString());
